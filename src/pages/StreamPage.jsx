2a7624119fd7b92323bd3ebdae9d77ed57e6fe0f
Pogen <76914800+IPogenI@users.noreply.github.com>
2025-04-11 13:10:14 +0600

    Streaming Page Added

---
+++ src/pages/StreamPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Stream from '../components/Stream'
+
+const StreamPage = () => {
+  return (
+    <>
+        <div className='flex w-screen h-screen bg-gray-800'>
+            <Stream />
+        </div>  
+    </>
+  )
+}
+
+export default StreamPage
