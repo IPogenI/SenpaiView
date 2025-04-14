809ce5cea236611c4b693e4e15117f529ae53ea0
Pogen <76914800+IPogenI@users.noreply.github.com>
2025-04-14 22:59:49 +0600

    Implementing Stream Logic

--- server/routes/anime.routes.js
+++ server/routes/anime.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { fetchEpisodeLinks } from '../scraper.js';
 import {
   getAllAnime,
   getAnimeById,
@@ -8,6 +9,22 @@ import {
 } from '../controllers/anime.controller.js';
 
 const router = express.Router();
+
+// Get episodes
+router.get('/episodes', async (req, res) => {
+  const animeTitle = req.query.title;
+  if (!animeTitle) {
+    return res.status(400).json({ message: "Anime title is required" });
+  }
+
+  try {
+    const episodes = await fetchEpisodeLinks(animeTitle);
+    res.json({ episodes });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
+
 
 router.get('/', getAllAnime);
 router.get('/:id', getAnimeById);
