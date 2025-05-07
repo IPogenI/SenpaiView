5f682fc883d78a3c3c67bf0e093a26580b785389
Pogen <76914800+IPogenI@users.noreply.github.com>
2025-05-08 02:32:13 +0600

    axios fix try 4

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL || '/api',
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL || '/',
   withCredentials: true, // if you need cookies/auth
 });
 
